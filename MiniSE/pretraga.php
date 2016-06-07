5e7b352223421acbcc3c88fbadf0627c38d0e41f
Legatus1993 <nemanja.nisic93@gmail.com>
2016-06-07 18:33:31 +0200

    Mini search engine

---
+++ MiniSE/pretraga.php
@@ -0,0 +1,4 @@
+<form action="rezultat.php" method="GET">
+<input type="text" size="20" name="x"/>
+<input type="submit" value="Pretrazi"/>
+</form>
---
+++ MiniSE/rezultat.php
@@ -0,0 +1,6 @@
+<form method="GET">
+<input type="text" value="<?php echo $_GET["x"]; ?>"/>
+</form>
+
+<iframe src=" <?php echo "https://www.google.ba/search?client=opera&q=".$_GET["x"]."&sourceid=opera&ie=UTF-8&oe=UTF-8&gws_rd=cr&ei=wPRWV_zgIobWU86fr4gO"; ?>" />
+
