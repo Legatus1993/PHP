7133bf86d4418ea3bb6304c5c4984693420e1fcf
Legatus1993 <nemanja.nisic93@gmail.com>
2016-06-07 19:39:18 +0200

    v.011

--- kalkulator.php
+++ kalkulator.php
@@ -1,8 +1,8 @@
 <form action="" method="GET">
 <input type="text" name="x"/>
 <select name="operacija">
-  <option name="sabiranje" value="+">+</option>
-  <option name="oduzimanje" value="-">-</option>
+  <option name="sabiranje" value="+" selected >+</option>
+  <option name="oduzimanje" value="-" >-</option>
   <option name="mnozenje" value="*">*</option>
   <option name="dijeljenje" value="/">/</option>
 </select>
@@ -11,6 +11,7 @@
 <?php 
 $operacija=$_GET["operacija"];
 switch($operacija) {
+	
 	case "+" :
 	$rezultat=$_GET["x"]+$_GET["y"];
 	break;
@@ -23,8 +24,12 @@ switch($operacija) {
 	case "/" :
 	$rezultat=$_GET["x"]/$_GET["y"];
 	break;
+	 default:
+       $rezultat="Molimo izaberite operaciju";
 }
+
 ?>
 <input type="text" value="<?php echo $rezultat; ?>" />
 </form>
 
+ 
