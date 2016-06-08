57f1f72a0cd9e2f1d7f405cfe90b070f82355b7d
Legatus1993 <nemanja.nisic93@gmail.com>
2016-06-08 02:14:47 +0200

    Calculator with Session

---
+++ sesijskikalkulator.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+?>
+<form action="" method="GET">
+<input type="text" name="x"/>
+<select name="operacija">
+  <option name="sabiranje" value="+" selected >+</option>
+  <option name="oduzimanje" value="-" >-</option>
+  <option name="mnozenje" value="*">*</option>
+  <option name="dijeljenje" value="/">/</option>
+</select>
+<input type="text" name="y"/>
+<input type="submit" value="="/>
+<?php 
+$operacija=$_GET["operacija"];
+switch($operacija) {
+	
+	case "+" :
+	$rezultat=$_GET["x"]+$_GET["y"];
+	$_SESSION["racunanja".$rezultat] = strval($_GET["x"])." + ".strval($_GET["y"])." = ".strval($rezultat)."  ";
+	break;
+	case "-" :
+	$rezultat=$_GET["x"]-$_GET["y"];
+	$_SESSION["racunanja".$rezultat] = strval($_GET["x"])." + ".strval($_GET["y"])." = ".strval($rezultat)."  ";
+	break;
+	case "*" :
+	$rezultat=$_GET["x"]*$_GET["y"];
+	$_SESSION["racunanja".$rezultat] = strval($_GET["x"])." + ".strval($_GET["y"])." = ".strval($rezultat)."  "; 
+	case "/" :
+	$rezultat=$_GET["x"]/$_GET["y"];
+	$_SESSION["racunanja".$rezultat] = strval($_GET["x"])." + ".strval($_GET["y"])." = ".strval($rezultat)."  ";
+	break;
+	 default:
+       $rezultat="Molimo izaberite operaciju";
+}
+?>
+<input type="text" value="<?php echo $rezultat; ?>" />
+<textarea rows="4" cols="50">
+<?php
+foreach ($_SESSION as $rez) {
+	echo $rez;	
+}
+?>
+</textarea>
+</form>
+
+ 
