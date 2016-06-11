890df7ce3bc9c6a0f58c666db9d5ca32e7c223d2
Legatus1993 <nemanja.nisic93@gmail.com>
2016-06-12 00:32:17 +0200

    V 0.5
    Pretrazivanje rezultata, komentari php koda

--- cap/sesijskikalkulator.php
+++ cap/sesijskikalkulator.php
@@ -9,7 +9,7 @@
 session_start();
 ?>
 <!-- Forma za unosenje brojeva i biranje operacije -->
-<form action="" method="GET">
+<form action="" method="POST">
 <!-- Prvi broj -->
 <input type="text" name="x"/>
 <!-- Biranje operacije -->
@@ -22,35 +22,35 @@ session_start();
 <!-- Drugi broj -->
 <input type="text" name="y"/>
 <!-- Dugme submit (=) za izracunavanje -->
-<input type="submit" value="="/>
+<input type="submit" value="=" name="izracunaj"/>
 <?php 
-$greske = "";
-$rezultat="";
-if(isset($_GET["operacija"])) {
-$operacija=$_GET["operacija"];
-if(!empty($_GET["x"]) && !empty($_GET["y"]) && $_GET["x"] !== 0 && $_GET["y"] !== 0) {
-	if(is_numeric($_GET["x"]) && is_numeric($_GET["y"])) {
+$greske = "";             																								// Varijabla za moguce greske pri unosu
+$rezultat="";            																								//Varijabla za rezultate
+if(isset($_POST["izracunaj"])) {                            																//Provjera submit dugmica	
+$operacija=$_POST["operacija"];																							
+if(!empty($_POST["x"]) && !empty($_POST["y"]) && $_POST["x"] !== 0 && $_POST["y"] !== 0) {									//Provjera dali je ista uneseno u polja
+	if(is_numeric($_POST["x"]) && is_numeric($_POST["y"])) {																//Provjera dali su unijeti brojevi
 		
 		
 		
-switch($operacija) {
+switch($operacija) {  																									//Racunanje i unos u sesiju
 	
 	case "+" :
-	$rezultat=$_GET["x"]+$_GET["y"];
-	$_SESSION["sabiranja"][] = strval($_GET["x"])." + ".strval($_GET["y"])." = ".strval($rezultat);
+	$rezultat=$_POST["x"]+$_POST["y"];
+	$_SESSION["sabiranja"][] = strval($_POST["x"])." + ".strval($_POST["y"])." = ".strval($rezultat);
 	break;
 	case "-" :
-	$rezultat=$_GET["x"]-$_GET["y"];
-	$_SESSION["oduzimanja"][] = strval($_GET["x"])." - ".strval($_GET["y"])." = ".strval($rezultat);
+	$rezultat=$_POST["x"]-$_POST["y"];
+	$_SESSION["oduzimanja"][] = strval($_POST["x"])." - ".strval($_POST["y"])." = ".strval($rezultat);
 	break;
 	case "*" :
-	$rezultat=$_GET["x"]*$_GET["y"];
-	$_SESSION["mnozenja"][] = strval($_GET["x"])." * ".strval($_GET["y"])." = ".strval($rezultat);
+	$rezultat=$_POST["x"]*$_POST["y"];
+	$_SESSION["mnozenja"][] = strval($_POST["x"])." * ".strval($_POST["y"])." = ".strval($rezultat);
 	break;
 	case "/" :
-	if( !empty($_GET["x"]) && !empty($_GET["y"]) ) {
-	$rezultat=$_GET["x"]/$_GET["y"];
-	$_SESSION["dijeljenja"][] = strval($_GET["x"])." / ".strval($_GET["y"])." = ".strval($rezultat);
+	if( !empty($_POST["x"]) && !empty($_POST["y"]) ) {
+	$rezultat=$_POST["x"]/$_POST["y"];
+	$_SESSION["dijeljenja"][] = strval($_POST["x"])." / ".strval($_POST["y"])." = ".strval($rezultat);
 	break;
 	}
 	else { $greske = "Dijeljenje sa nulom  nije definisano"; }
@@ -60,17 +60,18 @@ switch($operacija) {
 	}
 	}
 	
-else {
-	$greske = "Niste unijeli odgovarajuce vrijednosti.";
+else {																													//Izbacivanje greske ako uneseno nisu brojevi
+	$greske = "Niste unijeli odgovarajuce vrijednosti.";															
 }
 }
-else {
-	$greske = "Molimo unesite oba broja.";
+else {																													//Izbacivanje greske ako su polja prazna
+	$greske = "Molimo unesite oba broja.";																					
 }
+unset($_SERVER['QUERY_STRING']);
 }
 
 									
-$arrRacunanja = array();
+$arrRacunanja = array();																								//Niz koji upisuje vrijednosti sesije
 if(isset($_SESSION["sabiranja"])) {
 foreach ($_SESSION["sabiranja"] as $rez) {
     array_push($arrRacunanja, $rez);
@@ -96,18 +97,37 @@ foreach ($_SESSION["dijeljenja"] as $rez) {
 <!-- Rezultati racunanja -->
 <input type="text" value="<?php echo $rezultat; ?>" />
 
+<!-- Istorija i pretraga racunanja -->
+</form>
+<form action="" method="POST">
+<!-- Pretrazivanje istorije racunanja -->
+<input type="text" name="pretraga" placeholder="Unesite rezultat koji zelite da nadjete." size="40px"/>
+<input type="submit" name="pretrazi" value="Pronadji" />
 <!-- Istorija racunanja -->
-<textarea rows="10" cols="100">                                                                                  
-<?php
+<textarea rows="10" cols="100">                                                                                  		
+<?php																														//Izbacivanje istorije racunanja
 foreach ($arrRacunanja as $svi) {
     echo $svi;	
 	echo "\n";
 }
 ?>
 </textarea>
+<!-- Polje u kojem se pretrazuju rezultati pretrage -->
+<textarea rows="10" cols="100">                                                                                  		
+<?php							
+if(isset($_POST["pretrazi"])) {																							   //Pretrazivanje rezultata	
+foreach ($arrRacunanja as $rezovi) {
+    $pretragaipo = explode (" = ", $rezovi);
+	if($pretragaipo[1] == $_POST["pretraga"])
+    echo $rezovi;
+	echo "\n";
+}
+}
+?>
+</textarea>
 </form>
 <!-- Izbacivanje mogucih gresaka, ako budes mijenjao ili dodavao nesto dodaj iza size unutar " '' " -->
-<?php 
+<?php 																														//Izbacivanje mogucih gresaka
 if($greske !== "" ) {
 echo '<input type="text" name="greske" value="'.$greske.'" size="150px"/>';
 }
